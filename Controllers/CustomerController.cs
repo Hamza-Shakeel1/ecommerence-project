61a11a06a748e115694f747e16b8fcb41e3b2b5a
hamza shakeel <hamzashakeel219@gmail.com>
2024-12-20 18:51:48 +0500

    working on clint side

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using ecom.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecom.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private MyContext _myContext;
+
+        public CustomerController(MyContext myContext)
+        {
+            _myContext = myContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _myContext.tbl_categories.ToList();
+            ViewData["category"]= categories;
+            return View();
+        }
+    }
+}
